48568dc70e34902b7b8083855efd6803612e1ace
Riza Semih Koca <rsemihkoca@outlook.com>
2022-10-30 23:21:55 +0300

    added ProcFile

--- app/main.py
+++ app/main.py
@@ -1,15 +1,25 @@
 from fastapi import FastAPI
 
 from . import models
+from fastapi.middleware.cors import CORSMiddleware
 
-from .database import engine, get_db, table_exists
 from .routers import post, user, auth, vote
 from .configs import settings
 
 
-models.Base.metadata.create_all(bind=engine)
+#models.Base.metadata.create_all(bind=engine) instead of sqlalchemy alembic does this
 
 app = FastAPI()
+
+origins = ["*"] #Kendi domainini yaz
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
